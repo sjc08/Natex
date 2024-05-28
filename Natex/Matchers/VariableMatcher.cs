aede6b839e6024ed9f5dd876b4f765fc1b3ebd26
AlexSJC <cn_sjc@outlook.com>
2024-05-28 23:24:55 +0800

    update VariableMatcher

--- Natex/Matchers/VariableMatcher.cs
+++ Natex/Matchers/VariableMatcher.cs
@@ -5,29 +5,27 @@ namespace Asjc.Natex.Matchers
     /// <summary>
     /// A NatexMatcher for replacing variables.
     /// </summary>
-    public class VariableMatcher : NatexMatcher<object, string>
+    public class VariableMatcher : NatexMatcher
     {
-        public List<(string, string)> Variables { get; set; } =
+        public List<(string, Func<string>)> Variables { get; set; } =
         [
-            ("Now", DateTime.Now.ToString()),
-            ("UtcNow", DateTime.UtcNow.ToString()),
-            ("Today", DateTime.Today.ToString()),
-            ("Random", new Random().Next().ToString()),
-            ("MachineName", Environment.MachineName),
-            ("UserName", Environment.UserName)
+            ("Now", DateTime.Now.ToString),
+            ("UtcNow", DateTime.UtcNow.ToString),
+            ("Today", DateTime.Today.ToString),
+            ("Random", new Random().Next().ToString),
+            ("MachineName", ()=> Environment.MachineName),
+            ("UserName",()=> Environment.UserName)
         ];
 
-        public override string? Parse(Natex natex)
+        public override bool? Match(object value, Natex natex)
         {
-            var str = natex.Pattern;
+            string str = natex.Pattern;
             foreach (var item in Variables)
-                str = str.Replace($"<{item.Item1}>", item.Item2, natex.CaseInsensitive);
-            return str == natex.Pattern ? null : str; // Be careful not to make circular calls.
-        }
-
-        public override bool? Match(object value, string data, Natex natex)
-        {
-            return new Natex(data, natex).Match(value);
+                str = str.Replace($"<{item.Item1}>", item.Item2(), natex.CaseInsensitive);
+            // Be careful not to make circular calls.
+            natex = new(str, natex);
+            natex.Matchers.Remove(this);
+            return natex.Match(value);
         }
     }
 }
