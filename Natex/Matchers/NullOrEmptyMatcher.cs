922a397dcb8f7d82a59969ed6809979facc062f9
AlexSJC <cn_sjc@outlook.com>
2024-07-29 22:20:39 +0800

    update

--- Natex/Matchers/NullOrEmptyMatcher.cs
+++ Natex/Matchers/NullOrEmptyMatcher.cs
@@ -1,4 +1,4 @@
-﻿using Asjc.Extensions;
+﻿using Asjc.Utils.Extensions;
 using System.Collections;
 
 namespace Asjc.Natex.Matchers
--- Natex/Matchers/VariableMatcher.cs
+++ Natex/Matchers/VariableMatcher.cs
@@ -1,4 +1,4 @@
-﻿using Asjc.Extensions;
+﻿using Asjc.Utils.Extensions;
 
 namespace Asjc.Natex.Matchers
 {
